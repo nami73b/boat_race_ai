2df032266821fbe34ba6a2a1610267d817ec0ae3
yuta_miyawaki <miyawakiyuuta@miyawakiyuutanoMacBook-Pro.local>
2019-11-25 01:20:27 +0900

    first_commit

---
+++ crawling_boat.py
@@ -0,0 +1,247 @@
+import datetime
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from selenium import webdriver
+import sys
+import traceback
+
+def get_grade(class_names):
+    if ['heading2_title', 'is-ippan'] in class_names:
+        return '一般'
+    elif ['heading2_title', 'is-G3b'] in class_names:
+        return 'G3'
+    elif ['heading2_title', 'is-G3b'] in class_names:
+        return 'G2'
+    elif ['heading2_title', 'is-G3b'] in class_names:
+        return 'G1'
+    elif ['heading2_title', 'is-SGa'] in class_names:
+        return 'SG'
+    else:
+        np.nan
+
+def get_data_syussou_hyo(soup, race_date, place_id, race_no):
+    race_dct = {}
+    player_ar = []
+    
+    race_dct['race_date'] = race_date # 開催日付
+    race_dct['place_id'] = place_id # 場コード
+    race_dct['race_no'] = race_no # レース番号
+
+    race_dct['race_name'] = soup.find_all('h2', {'class': 'heading2_titleName'})[0].get_text().replace('\u3000', ' ')
+    race_dct['race_grade'] = get_grade([d.get('class') for d in soup.find_all('div')])
+    race_dct['race_grade'] = soup.find_all('span', {'class': 'heading2_titleDetail is-type1'})[0].get_text().split()[0]
+    race_dct['distance'] = soup.find_all('span', {'class': 'heading2_titleDetail is-type1'})[0].get_text().split()[1]
+
+    for rows in soup.find_all('tbody', {'class': ' is-fs12'}):
+        player_tmp = {}
+        player_tmp['is_miss'] = 'False'
+        player_tmp['bracket_no'] = str(int(rows.find_all('td')[0].get_text()))
+        tmp_cell = rows.find_all('td')[2]
+        player_tmp['player_id'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[0].get_text().split()[0]
+        player_tmp['player_grade'] = tmp_cell.find_all('span')[0].get_text()
+        player_tmp['player_name'] = tmp_cell.find_all('div', {'class': 'is-fs18 is-fBold'})[0].find_all('a')[0].get_text().replace('\u3000', '')
+        player_tmp['branch'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[0].split('/')[0]
+        player_tmp['born_area'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[0].split('/')[1]
+        player_tmp['age'] = int(tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[1].split('/')[0].replace('歳', ''))
+        player_tmp['f_count'] = int(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[0].replace('F', ''))
+        player_tmp['l_count'] = int(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[1].replace('L', ''))
+        player_tmp['start_time_avg'] = float(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[2])
+        player_tmp['first_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[0])
+        player_tmp['second_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[1])
+        player_tmp['third_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[2])
+        player_tmp['first_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[0])
+        player_tmp['second_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[1])
+        player_tmp['third_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[2])
+        player_tmp['motor_no'] = int(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[0])
+        player_tmp['motor_within_second_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[1])
+        player_tmp['motor_within_third_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[2])
+        player_tmp['boat_no'] = int(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[0])
+        player_tmp['boat_within_second_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[1])
+        player_tmp['boat_within_third_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[2])
+        
+        player_ar.append(player_tmp)
+        
+    for rows in soup.find_all('tbody', {'class': ' is-miss is-fs12'}):
+        player_tmp = {}
+        player_tmp['is_miss'] = 'False'
+        player_tmp['bracket_no'] = rows.find_all('td')[0].get_text()
+        tmp_cell = rows.find_all('td')[2]
+        player_tmp['player_id'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[0].get_text().split()[0]
+        player_tmp['player_grade'] = tmp_cell.find_all('span')[0].get_text()
+        player_tmp['player_name'] = tmp_cell.find_all('div', {'class': 'is-fs18 is-fBold'})[0].find_all('a')[0].get_text().replace('\u3000', '')
+        player_tmp['branch'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[0].split('/')[0]
+        player_tmp['born_area'] = tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[0].split('/')[1]
+        player_tmp['age'] = int(tmp_cell.find_all('div', {'class': 'is-fs11'})[1].get_text().split()[1].split('/')[0].replace('歳', ''))
+        player_tmp['f_count'] = int(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[0].replace('F', ''))
+        player_tmp['l_count'] = int(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[1].replace('L', ''))
+        player_tmp['start_time_avg'] = float(rows.find_all('td', {'class': 'is-lineH2'})[0].get_text().split()[2])
+        player_tmp['first_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[0])
+        player_tmp['second_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[1])
+        player_tmp['third_count_all'] = float(rows.find_all('td', {'class': 'is-lineH2'})[1].get_text().split()[2])
+        player_tmp['first_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[0])
+        player_tmp['second_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[1])
+        player_tmp['third_count_area'] = float(rows.find_all('td', {'class': 'is-lineH2'})[2].get_text().split()[2])
+        player_tmp['motor_no'] = int(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[0])
+        player_tmp['motor_within_second_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[1])
+        player_tmp['motor_within_third_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[3].get_text().split()[2])
+        player_tmp['boat_no'] = int(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[0])
+        player_tmp['boat_within_second_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[1])
+        player_tmp['boat_within_third_rate'] = float(rows.find_all('td', {'class': 'is-lineH2'})[4].get_text().split()[2])
+        
+        player_ar.append(player_tmp)
+        
+    return [race_dct, player_ar]
+
+def get_data_justbefore(soup, race_date, place_id, race_no):
+    player_ar = []
+    player_tmp = {}
+    race_dct = {}
+    pre_start_dct = {}
+    race_dct['weather'] = soup.find_all('span', {'class': 'weather1_bodyUnitLabelTitle'})[1].get_text()
+    race_dct['temperature'] = soup.find_all('span', ('class', 'weather1_bodyUnitLabelData'))[0].get_text()
+    race_dct['wind'] = soup.find_all('span', ('class', 'weather1_bodyUnitLabelData'))[1].get_text()
+    race_dct['water_temperature'] = soup.find_all('span', ('class', 'weather1_bodyUnitLabelData'))[2].get_text()
+    race_dct['wave_height'] = soup.find_all('span', ('class', 'weather1_bodyUnitLabelData'))[3].get_text()
+    
+    for pre in soup.find_all('tbody', {'class': 'is-p10-0'})[0].find_all('tr'):
+        pre_start = pre.get_text().split()
+        if pre_start[0] != '':
+            pre_start_dct[pre_start[0]] = pre_start[-1]
+    
+    for rows, pre in zip(soup.find_all('tbody', {'class': 'is-fs12 '}), soup.find_all('tbody', {'class': 'is-p10-0'})[0].find_all('tr')):
+        player_tmp = {}
+        player_tmp['bracket_no'] = rows.find_all('td')[0].get_text()
+        player_tmp['weight'] = float(rows.find_all('td')[3].get_text().replace('kg', ''))
+        player_tmp['pre_time'] = float(rows.find_all('td')[4].get_text())
+        player_tmp['tilt_angle'] = float(rows.find_all('td')[5].get_text())
+        player_tmp['propeller'] = rows.find_all('td')[6].get_text().replace('\xa0', '')
+        player_tmp['parts'] = rows.find_all('td')[7].get_text().replace('\n', '')
+        player_tmp['adjust_weight'] = rows.find_all('tr')[2].find_all('td')[0].get_text()
+        player_tmp['pre_start_timing'] = pre_start_dct[player_tmp['bracket_no']]
+        
+        player_ar.append(player_tmp)
+    return [race_dct, player_ar]
+
+def get_data_result(soup, race_date, place_id, race_no):
+    player_ar = []
+    player_tmp = {}
+    race_dct = {}
+    
+    table = soup.find_all('table', {'class': 'is-w495'})[2]
+    for row, bet_type in zip(table.find_all('tbody'), ['trifecta', 'trio', 'exacta', 'quinella', 'wide', 'win', 'place']):
+        tmp = []
+        for text in row.find_all('div', {'class': 'numberSet1_row'}):
+            if text.get_text().replace('\n', '') != '':
+                tmp.append(text.get_text().replace('\n', ''))
+        race_dct[bet_type+'_cmb'] = tmp
+        tmp = []
+        for text in row.find_all('span', {'class': 'is-payout1'}):
+            if text.get_text().replace('\n', '') != '\xa0':
+                tmp.append(text.get_text().replace('\n', ''))
+        race_dct[bet_type+'_payoff'] = tmp
+    
+    start_dct = {}
+    for bracket, start in zip(soup.find_all('table', {'class': 'is-w495'})[1].find_all('tbody')[0].find_all('span', {'class': 'table1_boatImage1Number'}), soup.find_all('table', {'class': 'is-w495'})[1].find_all('tbody')[0].find_all('span', {'class': 'table1_boatImage1TimeInner '})):
+        start_dct[bracket.get_text()] = start.get_text().split() if len(start.get_text().split()) == 2 else start.get_text().split() + ['']
+        
+    table = soup.find_all('table', {'class': 'is-w495'})[0]
+    for row in table.find_all('tbody'):
+        player_tmp = {}
+        player_tmp['bracket_no'] = row.find_all('td')[1].get_text()
+        player_tmp['order'] = row.find_all('td', {'class', 'is-fs14'})[0].get_text()
+        player_tmp['player_race_time'] = row.find_all('td')[3].get_text().replace('\'', ':').replace('"', ':')
+        player_tmp['start_timing'] = start_dct[row.find_all('td')[1].get_text()][0]
+        player_tmp['win_pattern'] = start_dct[row.find_all('td')[1].get_text()][1]
+        
+        player_ar.append(player_tmp)
+
+    return [race_dct, player_ar]
+
+def get_course_list(race_date, driver):
+    url = 'https://www.boatrace.jp/owpc/pc/race/index?hd={}'
+    url = url.format(race_date)
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+
+    course_list = []
+    for row in soup.find_all('td', {'class': 'is-arrow1 is-fBold is-fs15'}):
+        course_list.append(row.find_all('img')[0].get('src').split('_')[-1].replace('.png', ''))
+        
+    return course_list
+
+def get_page_source(url, driver):
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+
+    return soup
+    
+def main():
+    # 開始日付指定
+    dt = datetime.datetime(2019,11,23)
+
+    # ドライバーの読み込み
+    driver = webdriver.Chrome('./chromedriver')
+
+    # 出走表URL
+    syussou_hyo_url = 'https://boatrace.jp/owpc/pc/race/racelist?rno={race_no}&jcd={place_id}&hd={race_date}'
+    # 直前情報URL
+    justbefore_url = 'https://boatrace.jp/owpc/pc/race/beforeinfo?rno={race_no}&jcd={place_id}&hd={race_date}'
+    # 結果情報URL
+    result_url = 'http://boatrace.jp/owpc/pc/race/raceresult?rno={race_no}&jcd={place_id}&hd={race_date}'
+
+    for place_id in get_course_list(dt.strftime('%Y%m%d'), driver):
+        for race_no in [str(i+1) for i in range(12)]:
+            race_data = {}
+            player_data = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}}
+
+            url = syussou_hyo_url.format(**{'race_date': dt.strftime('%Y%m%d'), 'race_no': race_no, 'place_id': place_id})
+            soup = get_page_source(url, driver)
+
+            # 開催データがない場合の処理
+            # if soup.find('データがありません') != -1:
+            #     print('test')
+            #     break
+
+            race_date_tmp, player_data_tmp = get_data_syussou_hyo(soup, dt.strftime('%Y%m%d'), place_id, race_no)
+            for key, value in race_date_tmp.items():
+                race_data[key] = value
+            for player in player_data_tmp:
+                bracket_no = player['bracket_no']
+                for key, value in (player.items()):
+                    player_data[bracket_no][key] = value
+
+            url = justbefore_url.format(**{'race_date': dt.strftime('%Y%m%d'), 'race_no': race_no, 'place_id': place_id})
+            soup = get_page_source(url, driver)
+
+            race_date_tmp, player_data_tmp = get_data_justbefore(soup, dt.strftime('%Y%m%d'), place_id, race_no)
+            for key, value in race_date_tmp.items():
+                race_data[key] = value
+            for player in player_data_tmp:
+                bracket_no = player['bracket_no']
+                for key, value in (player.items()):
+                    player_data[bracket_no][key] = value
+
+            url = result_url.format(**{'race_date': dt.strftime('%Y%m%d'), 'race_no': race_no, 'place_id': place_id})
+            soup = get_page_source(url, driver)
+
+            race_date_tmp, player_data_tmp = get_data_result(soup, dt.strftime('%Y%m%d'), place_id, race_no)
+            for key, value in race_date_tmp.items():
+                race_data[key] = value
+            for player in player_data_tmp:
+                bracket_no = player['bracket_no']
+                for key, value in (player.items()):
+                    player_data[bracket_no][key] = value
+            print(player_data)
+
+if __name__ == '__main__':
+
+    main()
+
+
+
+
+
